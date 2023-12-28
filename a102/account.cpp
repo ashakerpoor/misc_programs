a4abb6ce62ecec14d784a47d141d4346126a534a
ashakerpoor <alireza.shakerpoor@gmail.com>
2023-12-28 14:07:14 -0600

    Object Oriented Design of bank accounts.

---
+++ a102/account.cpp
@@ -0,0 +1,92 @@
+#include "account.h"
+#include <iostream>
+#include <string>
+
+// Account class
+int Account::AccNoGenerator = 10000;
+
+Account::Account(const std::string &name, float balance):
+Name(name), Balance(balance) {
+	AccNo = ++AccNoGenerator;
+}
+
+Account::~Account() {
+	std::cout << "~Account()\n";
+}
+
+const std::string Account::GetName() const {
+	return Name;
+}
+
+float Account::GetBalance() const {
+	return Balance;
+}
+
+int Account::GetAccNo() const {
+	return AccNo;
+}
+
+float Account::GetInterestRate() const {
+	return 0.0f; //Gets implemented by child classes.
+}
+
+void Account::AccumulateInterest() {
+	Balance = 0.0f;	//Gets implemented by child classes.
+}
+
+void Account::Withdraw(float amount) {
+	if (amount <= Balance)
+		Balance -= amount;
+	else {
+		std::cout << "Insufficient Balance\n";
+	}
+}
+
+void Account::Deposit(float amount) {
+	Balance += amount;
+}
+
+// Savings class
+Savings::Savings(const std::string &name, float balance, float rate) :
+Account(name, balance), InterestRate(rate) {
+}
+
+Savings::~Savings() {
+	std::cout << "~Savings()\n";
+}
+
+float Savings::GetInterestRate() const {
+	return InterestRate;
+}
+
+void Savings::AccumulateInterest() {
+	Balance += Balance * InterestRate;
+}
+
+// Checking class
+//Checking::Checking(const std::string &name, float balance) :
+//Account(name, balance) {
+//}
+
+Checking::~Checking() {
+	std::cout << "~Checking()\n";
+}
+
+void Checking::Withdraw(float amount) {
+	if (GetBalance() - amount > 500.0f)	// Make sure the balance does not fall below 500.
+		// Balance -= amount;
+		Account::Withdraw(amount);
+	else {
+		std::cout << "Insufficient Balance\n";
+	}
+}
+
+void Transaction (Account *pTransAcc, float dAmount, float wAmount) {
+	std::cout << "Transaction requested." << std::endl;
+	std::cout << "Account #: " << pTransAcc->GetAccNo() << std::endl;
+	std::cout << "Initial balance: " << pTransAcc->GetBalance() << std::endl;
+	pTransAcc->Deposit(dAmount);
+	pTransAcc->AccumulateInterest();
+	pTransAcc->Withdraw(wAmount);
+	std::cout << "Final balance: " << pTransAcc->GetBalance() << std::endl;
+}
---
+++ a102/account.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <string>
+
+class Account {
+	int AccNo;
+	std::string Name;
+	static int AccNoGenerator;
+
+protected:
+	float Balance;
+
+public:
+	Account(const std::string &name, float balance);
+	virtual ~Account();
+	const std::string GetName() const;
+	float GetBalance() const;
+	int GetAccNo() const;
+
+	virtual float GetInterestRate() const;
+	virtual	void AccumulateInterest();
+	virtual	void Withdraw(float amount);
+
+	void Deposit(float amount);
+};
+
+class Savings :
+	public Account {
+		float InterestRate;
+
+	public:
+		Savings(const std::string &name, float balance, float rate);
+		~Savings();
+		float GetInterestRate() const override;
+		void AccumulateInterest() override;
+};
+
+class Checking :
+	public Account {
+	public:
+	//	Checking(const std::string &name, float balance);
+		using Account::Account;
+		~Checking();
+		void Withdraw(float amount) override;
+};
+
+void Transaction (Account *pTransAcc, float dAmount, float wAmount);
